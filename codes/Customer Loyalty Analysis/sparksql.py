9c008cfe26f99aa25448687455083616e99739f4
bhoomisv <bhoomisv96@gmail.com>
2024-11-08 17:45:38 +0530

    Update sparksql.py

--- codes/Customer Loyalty Analysis/sparksql.py
+++ codes/Customer Loyalty Analysis/sparksql.py
@@ -1,3 +1,157 @@
+from pyspark.sql import SparkSession, functions as F
+from pyspark.sql.functions import col, lit, when, datediff, sum, when, col, max, avg, min
+spark = SparkSession.builder.appName("my code practice").getOrCreate()
+
+customer_loyalty = [
+("Customer1", 25, 700),
+("Customer2", 15, 400),
+("Customer3", 5, 50),
+("Customer4", 18, 450),
+("Customer5", 22, 600),
+("Customer6", 2, 80),
+("Customer7", 12, 300),
+("Customer8", 6, 150),
+("Customer9", 10, 200),
+("Customer10", 1, 90)
+]
+customer_loyalty_df = spark.createDataFrame(customer_loyalty, ["customer_name", "purchase_frequency", "average_spending"])
+
+customer_loyalty_df.createOrReplaceTempView("customer_loyalty_table")
+
+result_df = spark.sql("""
+
+""")
+result_df.show()
+result_df1 = spark.sql("""
+
+""")
+result_df1.show()
+result_df2 = spark.sql("""
+
+""")
+result_df2.show()
+result_df3 = spark.sql("""
+
+""")
+result_df3.show()
+result_df4 = spark.sql("""
+
+""")
+result_df4.show()
+
+-- Classify customers
+SELECT
+  customer_name,
+  purchase_frequency,
+  average_spending,
+  CASE
+    WHEN purchase_frequency > 20 AND average_spending > 500 THEN 'Highly Loyal'
+    WHEN purchase_frequency BETWEEN 10 AND 20 THEN 'Moderately Loyal'
+    ELSE 'Low Loyalty'
+  END AS loyalty_level
+FROM
+  customer_loyalty_table;
+
+-- Count customers by loyalty level
+SELECT
+  loyalty_level,
+  COUNT(*) AS customer_count
+FROM
+  (
+    SELECT
+      customer_name,
+      purchase_frequency,
+      average_spending,
+      CASE
+        WHEN purchase_frequency > 20 AND average_spending > 500 THEN 'Highly Loyal'
+        WHEN purchase_frequency BETWEEN 10 AND 20 THEN 'Moderately Loyal'
+        ELSE 'Low Loyalty'
+      END AS loyalty_level
+    FROM
+      customer_loyalty_table
+  )
+GROUP BY
+  loyalty_level;
+
+-- Calculate average spending for Highly Loyal customers
+SELECT
+  AVG(average_spending) AS avg_spending
+FROM
+  (
+    SELECT
+      customer_name,
+      purchase_frequency,
+      average_spending,
+      CASE
+        WHEN purchase_frequency > 20 AND average_spending > 500 THEN 'Highly Loyal'
+        WHEN purchase_frequency BETWEEN 10 AND 20 THEN 'Moderately Loyal'
+        ELSE 'Low Loyalty'
+      END AS loyalty_level
+    FROM
+      customer_loyalty_table
+  )
+WHERE
+  loyalty_level = 'Highly Loyal';
+
+-- Calculate minimum spending for Moderately Loyal customers
+SELECT
+  MIN(average_spending) AS min_spending
+FROM
+  (
+    SELECT
+      customer_name,
+      purchase_frequency,
+      average_spending,
+      CASE
+        WHEN purchase_frequency > 20 AND average_spending > 500 THEN 'Highly Loyal'
+        WHEN purchase_frequency BETWEEN 10 AND 20 THEN 'Moderately Loyal'
+        ELSE 'Low Loyalty'
+      END AS loyalty_level
+    FROM
+      customer_loyalty_table
+  )
+WHERE
+  loyalty_level = 'Moderately Loyal';
+
+-- Identify Low Loyalty customers with average spending < 100 and frequency < 5
+SELECT
+  *
+FROM
+  (
+    SELECT
+      customer_name,
+      purchase_frequency,
+      average_spending,
+      CASE
+        WHEN purchase_frequency > 20 AND average_spending > 500 THEN 'Highly Loyal'
+        WHEN purchase_frequency BETWEEN 10 AND 20 THEN 'Moderately Loyal'
+        ELSE 'Low Loyalty'
+      END AS loyalty_level
+    FROM
+      customer_loyalty_table
+  )
+WHERE
+  loyalty_level = 'Low Loyalty' AND average_spending < 100 AND purchase_frequency < 5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 customer_loyalty_df = customer_loyalty_df.withColumn("loyalty_level",when((col("purchase_frequency") > 20) & (col("average_spending") > 500), "Highly Loyal").when((col("purchase_frequency") >= 10) & (col("purchase_frequency") <= 20), "Moderately Loyal").otherwise("Low Loyalty"))
 
